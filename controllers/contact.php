2bbd187075af119eacb0b0b7244b26dff0390c1a
William Davies <wd6g14@soton.ac.uk>
2016-11-28 01:20:19 +0000

    Fixed value to "to" parameter on server side

--- controllers/contact.php
+++ controllers/contact.php
@@ -7,8 +7,8 @@ class Contact extends Controller {
 			extract($this->request->data);
 			$from = "From: $from";
 
+			$to = $this->Model->Settings->getSetting('email');	//defining who the request is sent to here rather than in the html, where it could be edited
 			
-			$to = $site['email'];
 			mail($to,$subject,$message,$from);
 
 			StatusMessage::add('Thank you for contacting us');
